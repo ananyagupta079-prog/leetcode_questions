c70193f005db98947b1fdfa9d1c3221415d79de8
ananyagupta079-prog <ananyagupta079@gmail.com>
2026-08-02 14:18:55 +0530

    maximum_ones

---
+++ 485.maximum_consecutive_ones.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxi=0,cnt=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                cnt++;
+            }
+            else{
+                cnt=0;
+            }
+            maxi=max(maxi,cnt);
+        }
+        return maxi;
+}
